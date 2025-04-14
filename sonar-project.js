eae891061d208346b3e7fb15bb8dc2320583f83d
Tejasree123235 <sree60885@gmail.com>
2025-04-14 20:13:54 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.19.152:9000/',
+  serverUrl: 'http://172.31.28.38:7842/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_3898cfbc0d999be7ad06a9495e08ca0890007cf8',
+	    'sonar.login': '4969d4f244d16fa64535e56960245f1bfcd61e29',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
